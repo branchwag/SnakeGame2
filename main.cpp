af3ec56bd5c2a8bf6b6d06d7459698b649f7f0cc
LeAnne B <branchleannelmb@gmail.com>
2024-03-08 21:55:48 -0500

    allows snake to move a little

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <raylib.h>
 #include <deque>
+#include <raymath.h>
 
 using namespace std;
 
@@ -11,10 +12,24 @@ int cellSize = 30;
 int cellCount = 25;
 int offset = 75;
 
+double lastUpdateTime = 0;
+
+bool EventTriggered(double interval)
+{
+    double currentTime = GetTime();
+    if (currentTime - lastUpdateTime >= interval)
+    {
+        lastUpdateTime = currentTime;
+        return true;
+    }
+    return false;
+}
+
 class Snake {
     
     public :
         deque<Vector2> body = { Vector2{6,9}, Vector2{5,9}, Vector2{4,9} };
+        Vector2 direction = {1,0};
 
         void Draw() {
 
@@ -25,6 +40,11 @@ class Snake {
                 Rectangle segment = Rectangle{offset + x * cellSize, offset + y * cellSize, (float)cellSize, (float)cellSize};
                 DrawRectangleRounded(segment, 0.5, 6, darkGreen);
             }
+        }
+
+        void Update() {
+            body.pop_back();
+            body.push_front(Vector2Add(body[0], direction));
         }
 
 };
@@ -72,6 +92,11 @@ int main () {
     while(WindowShouldClose() == false) {
 
         BeginDrawing();
+
+        if(EventTriggered(0.2)){
+            snake.Update();
+        }
+        //snake.Update();
 
         ClearBackground(green);
         food.Draw();
