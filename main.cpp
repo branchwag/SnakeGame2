db4a9027764681bf2ad54f3004fe0f82ae5baa05
LeAnne B <branchleannelmb@gmail.com>
2024-03-09 12:11:50 -0500

    refactors by adding game class

--- main.cpp
+++ main.cpp
@@ -80,54 +80,69 @@ class Food {
 
 };
 
+class Game {
+    public: 
+    Snake snake = Snake();
+    Food food = Food(); 
+
+    void Draw() {
+        food.Draw();
+        snake.Draw();
+    }
+
+    void Update () {
+        snake.Update();
+    }
+};
+
 int main () {
 
     cout << "Starting the game" << endl;
     InitWindow(cellSize * cellCount, cellSize * cellCount, "Retro Snake");
     SetTargetFPS(60);
 
-    Food food = Food();
-    Snake snake = Snake();
+    // Food food = Food();
+    // Snake snake = Snake();
+    Game game = Game();
 
     while(WindowShouldClose() == false) {
 
         BeginDrawing();
 
         if(EventTriggered(0.2)){
-            snake.Update();
+            game.Update();
         }
 
         //KEY_UP OR W
-        if(IsKeyPressed(KEY_UP) || IsKeyPressed(KEY_W)) {
+        if((IsKeyPressed(KEY_UP) || IsKeyPressed(KEY_W)) && game.snake.direction.y != 1) {
 
-            snake.direction = {0, -1};
+            game.snake.direction = {0, -1};
 
         }
         
         //KEY_DOWN or S
-        if(IsKeyPressed(KEY_DOWN) || IsKeyPressed(KEY_S)) {
+        if((IsKeyPressed(KEY_DOWN) || IsKeyPressed(KEY_S)) && game.snake.direction.y != -1) {
 
-            snake.direction = {0, 1};
+            game.snake.direction = {0, 1};
     
         }
 
         //KEY_LEFT or A
-        if(IsKeyPressed(KEY_LEFT) || IsKeyPressed(KEY_A)) {
+        if((IsKeyPressed(KEY_LEFT) || IsKeyPressed(KEY_A)) && game.snake.direction.x != 1) {
 
-            snake.direction = {-1, 0};
+            game.snake.direction = {-1, 0};
     
         }
 
-        //KEY)RIGHT or D
-        if(IsKeyPressed(KEY_RIGHT) || IsKeyPressed(KEY_D)) {
+        //KEY_RIGHT or D
+        if((IsKeyPressed(KEY_RIGHT) || IsKeyPressed(KEY_D)) && game.snake.direction.x != -1) {
 
-            snake.direction = {1, 0};
+            game.snake.direction = {1, 0};
     
         }
 
         ClearBackground(green);
-        food.Draw();
-        snake.Draw();
+        game.Draw();
 
         EndDrawing();
     }
